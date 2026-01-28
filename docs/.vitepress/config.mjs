7227ce3d308844fee0c7cfd5cd89fa8436bd3ceb
江大爷 <84177566+baidu8@users.noreply.github.com>
2026-01-28 22:57:59 +0800

    J-git: docs/.vitepress/config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,12 +1,11 @@
-export 默认 {
-  base: '/VitePress/', // 必须保留，否则样式会乱
+export default {
+  base: '/VitePress/',
   title: "江大爷博客",
   description: "梦到什么说什么",
   themeConfig: {
     nav: [
       { text: '首页', link: '/' }
     ],
-    // 侧边栏也可以保留一个简单的自动项，或者干脆留空，让首页承担所有功能
     socialLinks: [
       { icon: 'github', link: 'https://github.com/baidu8' }
     ]
